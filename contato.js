a5358631624c4d90cad15612c88e52a84bdd0c6a
unknown <gabrielkuffellima@gmail.com>
2026-04-12 17:12:21 -0300

    Atualização do projeto

--- contato.js
+++ contato.js
@@ -5,8 +5,4 @@ event.preventDefault();
 document.getElementById("mensagemSucesso").innerHTML =
 "Mensagem enviada com sucesso! Agradeço por sua mensagem!!";
 
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 6bf2dc1391f487ddd161af6c1b786ff426670350
